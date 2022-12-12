5ee6f3f0e53cd3094656149ac689f322ffb618b8
Daniil Glebov <dania.57.v@gmail.com>
2022-12-13 03:55:12 +0400

    Iter6, add file storage usage

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,12 +4,13 @@ import (
 	"github.com/tank4gun/gourlshortener/internal/app/server"
 	"github.com/tank4gun/gourlshortener/internal/app/storage"
 	"log"
+	"os"
 )
 
 func main() {
 	internalStorage := map[uint]string{}
 	nextIndex := uint(1)
-	strg := &storage.Storage{InternalStorage: internalStorage, NextIndex: nextIndex}
+	strg, _ := storage.NewStorage(internalStorage, nextIndex, os.Getenv("FILE_STORAGE_PATH"))
 	currentServer := server.CreateServer(strg)
 	log.Fatal(currentServer.ListenAndServe())
 }
--- internal/app/storage/storage.go
+++ internal/app/storage/storage.go
@@ -1,6 +1,10 @@
 package storage
 
-import "errors"
+import (
+	"encoding/json"
+	"errors"
+	"os"
+)
 
 type Repository interface {
 	InsertValue(value string) error
@@ -11,6 +15,44 @@ type Repository interface {
 type Storage struct {
 	InternalStorage map[uint]string
 	NextIndex       uint
+	Encoder         *json.Encoder
+	Decoder         *json.Decoder
+}
+
+type MapItem struct {
+	Key   uint
+	Value string
+}
+
+func Max(x, y uint) uint {
+	if x < y {
+		return y
+	}
+	return x
+}
+
+func NewStorage(internalStorage map[uint]string, nextInd uint, filename string) (*Storage, error) {
+	if filename == "" {
+		return &Storage{internalStorage, nextInd, nil, nil}, nil
+	} else {
+		file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0777)
+		if err != nil {
+			return nil, err
+		}
+		internalStorage := make(map[uint]string)
+		decoder := json.NewDecoder(file)
+		encoder := json.NewEncoder(file)
+		nextInd := uint(0)
+		for {
+			var mapItem MapItem
+			err := decoder.Decode(&mapItem)
+			if err != nil {
+				return &Storage{internalStorage, nextInd, encoder, decoder}, nil
+			}
+			internalStorage[mapItem.Key] = mapItem.Value
+			nextInd = Max(nextInd, mapItem.Key)
+		}
+	}
 }
 
 func (strg *Storage) GetNextIndex() (uint, error) {
@@ -23,6 +65,10 @@ func (strg *Storage) InsertValue(value string) error {
 		return errors.New("got same key already in storage")
 	}
 	strg.InternalStorage[strg.NextIndex] = value
+	mapItem := MapItem{Key: strg.NextIndex, Value: value}
+	if err := strg.Encoder.Encode(mapItem); err != nil {
+		return err
+	}
 	strg.NextIndex++
 	return nil
 }
